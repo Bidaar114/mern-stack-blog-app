ad0007db4209dd34b741252736151d4411ba2a37
Ibrahim-Hussein <ihusein114@gmail.com>
2024-01-30 07:23:46 -0800

    updated all

--- fortend/vite.config.js
+++ fortend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy:{
-      '/api': "https://mern-stack-blog-app-beta.vercel.app/"
+      '/api': "http://localhost:8000/"
     }
   },
   resolve: {
